84ba5b7495b593e6e7f6d9c4db411be91e471fa0
Fahad Adeel <fahadadeel@gmail.com>
2016-04-26 17:02:09 +0500

    Aspose.Email Java for Python Examples

---
+++ Plugins/Aspose.Email Java for Python/aspose-email-java-for-python.py
@@ -0,0 +1,9 @@
+# To change this license header, choose License Headers in Project Properties.
+# To change this template file, choose Tools | Templates
+# and open the template in the editor.
+
+__author__ = "nhp09"
+__date__ = "$Feb 22, 2016 12:07:10 PM$"
+
+if __name__ == "__main__":
+    print "Hello World"
---
+++ Plugins/Aspose.Email Java for Python/setup.py
@@ -0,0 +1,19 @@
+__author__ = 'fahadadeel'
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'aspose-email-java-for-python',
+    packages = find_packages(),
+    version = '1.0',
+    description = 'Aspose.Email Java for Python is a project that demonstrates / provides the Aspose.Email for Java API usage examples in Python.',
+    author='Fahad Adeel',
+    author_email='email@aspose.com',
+    url='http://www.aspose.com/docs/display/emailjava/Aspose.Email+Java+for+Python',
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent'
+    ]
+)
