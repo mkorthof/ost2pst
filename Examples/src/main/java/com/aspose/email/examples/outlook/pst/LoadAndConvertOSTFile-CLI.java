539576900652e3438114c0d36e94bcd24aa6a8ea
Marius <m@mari.us.com>
2017-07-02 13:24:28 +0200

    initial commit

---
+++ Examples/src/main/java/com/aspose/email/examples/outlook/pst/LoadAndConvertOSTFile-CLI.java
@@ -0,0 +1,43 @@
+package com.aspose.email.examples.outlook.pst;
+
+import com.aspose.email.FileFormat;
+import com.aspose.email.FolderInfo;
+import com.aspose.email.FolderInfoCollection;
+import com.aspose.email.PersonalStorage;
+import com.aspose.email.examples.Utils;
+
+public class LoadAndConvertOSTFile {
+
+	public static String dataDir = Utils.getSharedDataDir(ExtractingNMessagesFromAPSTFile.class) + "outlook/";
+
+	public static void main(String[] args) {
+		//Read an OST file
+		readAnOSTFile();
+		
+		//Converting OST to PST
+		convertOSTToPST();
+	}
+
+	public static void readAnOSTFile() {
+		// Load the Outlook PST file
+//		String strPSTFile = dataDir + "Sample.ost";
+		String strPSTFile = "input.ost";
+		PersonalStorage pst = PersonalStorage.fromFile(strPSTFile);
+
+		// Get sub-folders of Root
+		FolderInfoCollection folderInfoCollection = pst.getRootFolder().getSubFolders();
+		// Loop over all the-sub folders
+		for (int i = 0; i < folderInfoCollection.size(); i++) {
+			// Display all the folders
+			FolderInfo folderInfo = (FolderInfo) folderInfoCollection.get_Item(i);
+			System.out.println(folderInfo.getDisplayName());
+		}
+	}
+
+	public static void convertOSTToPST() {
+//		PersonalStorage ost = PersonalStorage.fromFile(dataDir + "input.ost");
+		PersonalStorage ost = PersonalStorage.fromFile("input.ost");
+//		ost.saveAs(dataDir + "output.pst", FileFormat.Pst);
+		ost.saveAs("output.pst", FileFormat.Pst);
+	}
+}
