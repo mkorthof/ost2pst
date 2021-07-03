195357eddc4973c88a241500ff017d3a027b8d2f
Marius <m@mari.us.com>
2021-07-03 21:16:35 +0200

    aspose lib 21.5, fixes, prep for corrupted pst rec

--- README.md
+++ README.md
@@ -1,46 +1,83 @@
 # OST2PST
 
-A while ago I needed something to convert a 5GB Outlook OST file to PST format. Turned out Aspose has a good (commercial) API/library for Java to do exactly that. I had not touched Java for 10 years but got things sorted within a few hours thanks to clear instructions and good examples and docs. I just used the example and made minimal changes, nothing more.
+A while ago I needed something to convert a 5GB Outlook OST file to PST format. Turned out [Aspose](https://products.aspose.com/email) has a good (commercial) API/library for Java to do exactly that. I had not touched Java for 10 years but got things sorted within a few hours thanks to clear instructions and good examples and docs. I just used the example and made minimal changes, nothing more.
 
-[**Saving to Outlook 2013/2016 PST files is not supported**](https://docs.aspose.com/display/emailjava/Read+and+Convert+Outlook+OST+File#ReadandConvertOutlookOSTFile-ConvertingOSTtoPST)
-
+[**Saving to Outlook 2013/2016 PST files is not supported**](https://docs.aspose.com/email/java/read-and-convert-outlook-ost-file/#converting-ost-to-pst)
 
 ## Download
 
 Get files below or from [Releases page](../../releases/latest)
 
-### Jar and wrappers
+**Jar and wrappers:**
 
-* Main executable: [ost2pst.jar](ost2pst.jar) verify: [SHA512SUMS](SHA512SUMS)
+* Main executable: [ost2pst.jar](ost2pst.jar) (verify [SHA512SUMS](SHA512SUMS))
 * Windows batch file: [ost2pst.bat](ost2pst.bat)
 * Linux/BSD shell script: [ost2pst.sh](ost2pst.sh)
-* If needed download Java here: [JRE8](https://java.com/en/download/manual.jsp) or alternatively: [JavaSE-RE-8](http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html)
+
+
+If you need to install Java use your OS package manager or download it here: [JRE8](https://java.com/en/download/manual.jsp) or alternatively [JavaSE-RE-8](http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html).
 
 ### Usage
 
-Windows: `ost2pst.bat <input.ost> <output.pst>` _(same for .sh)_
+* Windows: `ost2pst.bat <input.ost> <output.pst>`
+* Linux: `ost2pst.sh <input.ost> <output.pst>`
 
-* you can also run the jar directly: `java -jar ost2pst.jar`
-* the jar needs JRE8
+Or run the jar directly: `java -jar ost2pst.jar <input.ost> <output.pst>`
+
+_Remember you need to have at least JRE8 installed, OpenJDK is untested but should work_
 
 ### Example
 
 ``` batch
+
 C:\src\ost2pst>ost2pst.bat input.ost output.pst
 
-OST2PST (200316)
+OST2PST (210619)
 
-INFO: Loading OST file "input.pst" (5GB)
+INFO: File format is "64-bit Unicode" (23)
+INFO: Loading file "input.pst" (5GB)
       Folder [00] "Public"
       Folder [01] "Inbox"
       Folder [02] "Outbox"
       Folder [03] "Sent Items"
 
-INFO: File format is "64-bit Unicode" (23)
 INFO: Converting "input.pst" to "output.ost" 511MB/4751MB (11%) |
+
+```
+
+## Building from source
+
+### Import into [Eclipse](https://www.eclipse.org):
+
+* [pom.xml](pom.xml)
+* [Loast2pstLoadAndConvertOSTFileCLI.java](src/main/java/com/ost2pst/LoadAndConvertOSTFileCLI.java)
+
+### Get Aspose.Email for Java API/lib (*required*)
+
+* [GitHub Aspose Email-for-Java](https://github.com/aspose-email/Aspose.Email-for-Java)
+* [docs.aspose.com Installation (Maven)](https://docs.aspose.com/display/emailjava/Installation)
+* [docs.aspose.com Aspose.Email java for Eclipse  (Maven)](https://docs.aspose.com/display/emailjava/Aspose.Email+Java+for+Eclipse+-+Maven)
+
+### Create runnable Jar in Eclipse
+
+* use Ant to 'Export', 'Java', 'Runnable JAR file' (used for [ost2pst.jar](ost2pst.jar))
+* or alternatively use Maven to build project
+
+## TODO
+
+Add new option that tries to read corrupted ost/pst files. See [here](https://docs.aspose.com/email/java/aspose-email-for-java-20-10-release-notes/#reading-corrupted-pstost-files) for more info.
+
+``` batch
+C:\src\ost2pst>ost2pst.bat -c broken.ost
 ```
 
 ## Changelog
+
+### 20210702
+
+* fixed file size formatting: [programming.guide](https://programming.guide/java/formatting-byte-size-to-human-readable-format.html) :)
+* updated aspose-email lib to 21.5
+* removed aspose-email submodule from git repo
 
 ### 20200317
 
@@ -64,29 +101,11 @@ INFO: Converting "input.pst" to "output.ost" 511MB/4751MB (11%) |
 * improved progress indicator
 * updated aspose-email lib to 18.8
 
-## Source
-
-### Import into [Eclipse](https://www.eclipse.org):
-
-* [pom.xml](pom.xml)
-* [Loast2pstLoadAndConvertOSTFileCLI.java](src/main/java/com/ost2pst/LoadAndConvertOSTFileCLI.java)
-
-### Aspose.Email for Java API/lib (*required*)
-
-* [GitHub Aspose Email-for-Java](https://github.com/aspose-email/Aspose.Email-for-Java)
-* [docs.aspose.com Installation (Maven)](https://docs.aspose.com/display/emailjava/Installation)
-* [docs.aspose.com Aspose.Email java for Eclipse  (Maven)](https://docs.aspose.com/display/emailjava/Aspose.Email+Java+for+Eclipse+-+Maven)
-  
-### Create runnable Jar in Eclipse
-
-* use Ant to 'Export', 'Java', 'Runnable JAR file' (used for [ost2pst.jar](ost2pst.jar))
-* or alternatively use Maven to build project
-  
 ## More info
 
-Aspose.Email for Java: https://products.aspose.com/email/java  
-What lead me to Aspose: http://wiki.opf-labs.org/pages/viewpage.action?pageId=25887031  
-Possible alternative https://github.com/rjohnsondev/java-libpst  
+* Aspose.Email for Java: <https://products.aspose.com/email/java>
+* What lead me to Aspose: <http://wiki.opf-labs.org/pages/viewpage.action?pageId=25887031>
+* Possible alternative <https://github.com/rjohnsondev/java-libpst>
 
 ---
 > moved from old repo mkorthof/Aspose.Email-for-Java
--- src/main/java/com/ost2pst/LoadAndConvertOSTFileCLI.java
+++ src/main/java/com/ost2pst/LoadAndConvertOSTFileCLI.java
@@ -1,26 +1,37 @@
 package com.ost2pst;
 
-/* https://github.com/aspose-email/Aspose.Email-for-Java */
+// https://github.com/aspose-email/Aspose.Email-for-Java 
+
 import com.aspose.email.FileFormat;
 import com.aspose.email.FolderInfo;
 import com.aspose.email.FolderInfoCollection;
 import com.aspose.email.PersonalStorage;
-
+import com.aspose.email.MapiMessage;
 //import com.aspose.email.examples.Utils;
+
 import java.io.File;
 import java.io.RandomAccessFile;
+import java.text.CharacterIterator;
+import java.text.StringCharacterIterator;
 
 public class LoadAndConvertOSTFileCLI {
-	
+
 	public static void showUsage(int rc) {
-		System.out.println("USAGE: ost2pst.jar <input.ost> <output.pst>");
+		System.out.println("USAGE: ost2pst.jar <input.ost> <output.pst>    convert ost to pst");
+		//System.out.println("       ost2pst.jar -c <input.ost> [-s <dir>]   read/save corrupted ost");
 		System.exit(rc);
 	}
 	
-	public static void main(String[] args) {		
+	public static void main(String[] args) {
 		String inputFile = "";
 		String outputFile = "";
+		File fIn = null;
+		File fOut = null;
 		boolean debug = false;
+		boolean corruptedOst = false;
+		boolean saveCorrupted = false;
+		// TODO: add -s <dir> to set dataDir
+        String dataDir = "outlook_restore";
 		// Print version from pom.xml
 		String sVer = String.format("OST2PST (%s)\n", Version.VERSION); 
 		System.out.println("");
@@ -29,27 +40,49 @@ public class LoadAndConvertOSTFileCLI {
 		if (args.length == 0) {
 			showUsage(0);
 		} else {
-			try { inputFile = args[0]; }
-			catch(Exception e) {
-				System.out.println("ERROR: no input file specified");
-				showUsage(2);
+		    for (String arg : args) {
+		        if (arg.equals("-h"))
+					showUsage(0);
+		        if (arg.equals("-d"))
+		        	debug = true;
+		        if (arg.equals("-c"))
+		        	corruptedOst = true; 
+		        if (arg.equals("-s"))
+		        	saveCorrupted = true;
+		        	// TODO: option for dataDir
+		    }
+			if (corruptedOst) {
+				try { inputFile = args[1]; }
+				catch(Exception e) {
+					System.out.println("ERROR: no input file specified");
+					showUsage(2);
+				}
+			} else {
+				try { inputFile = args[0]; }
+				catch(Exception e) {
+					System.out.println("ERROR: no input file specified");
+					showUsage(2);
+				}
+				try { outputFile = args[1]; }
+				catch(Exception e) {
+					System.out.println("ERROR: no output file specified");
+					showUsage(2);
+				}
+				fOut = new File(outputFile);
 			}
-			try { outputFile = args[1]; }
-			catch(Exception e) {
-				System.out.println("ERROR: no output file specified");
-				showUsage(2);
-			}
-			if (debug) {
- 				System.out.println("DEBUG: args.length=" + args.length + " args[0]=" + args[0] + " args[1]=" + args[1]);
- 			}
-		}
-		File fIn = new File(inputFile);	
-		File fOut = new File(outputFile);
-		if (debug) {
-			System.out.println("DEBUG: fIn.exists=" + fIn.exists() + " fIn.isFile=" + fIn.isFile());
+			fIn = new File(inputFile);	
 		}	
+		if (debug) {
+			System.out.printf("DEBUG: args.length=%s args[0]=\"%s\" args[1]=\"%s\"\n", args.length, args[0], args[1]);
+			if (args.length > 2)
+				System.out.printf("DEBUG: args[2]=%s\n", args[2]);
+			if (args.length > 3)
+				System.out.printf("DEBUG: args[3]=%s\n", args[3]);
+			System.out.printf("DEBUG: fIn.exists=%s fIn.isFile=%s\n",  fIn.exists(), fIn.isFile());
+			System.out.printf("DEBUG: corruptedOst=%s saveCorrupted=%s\n", corruptedOst, saveCorrupted);
+		}
 		// Check if input and output file exist
-		if ( !(fIn.exists() && fIn.isFile()) ) {
+		if ( !(fIn.exists() && fIn.isFile())) {
 			String sInErr = String.format("ERROR: input file \"%s\" does not exist", inputFile);
 			System.out.println(sInErr);
 			System.exit(2);
@@ -59,36 +92,59 @@ public class LoadAndConvertOSTFileCLI {
 			System.out.println(sInErr);
 			System.exit(2);
 		}
-		if ( fOut.exists() || fOut.isFile() ) {
-			String sOutErr = String.format("ERROR: output file \"%s\" already exists", outputFile);
-			System.out.println(sOutErr);
-			System.exit(2);
+		int fmtTypeNum = typeOSTFile(debug, inputFile);
+		String fmtTypeStr = fileFormatType(debug, fmtTypeNum);
+		System.out.printf("INFO: File format is \"%s\" (%d)\n", fmtTypeStr, fmtTypeNum);
+		System.out.printf("INFO: Loading file \"%s\" (%s)\n", inputFile, humanReadableByteCountSI(fIn.length()));
+		// Read corrupted OST file
+		if (corruptedOst) {
+			if (saveCorrupted) {
+				File dOut = new File(dataDir.toString());
+				if ( dOut.exists() && dOut.isDirectory()) {
+					System.out.printf("ERROR: Dir '%s' already exists, exiting...\n", dataDir);
+					System.exit(1);
+				} else {
+					dOut.mkdirs();
+					System.out.printf("INFO: Restoring readable messages from corrupted file to dir '%s'...\n", dataDir);
+				}
+			} else {
+				System.out.printf("INFO: Finding readable messages from corrupted file...\n");
+				dataDir = null;
+			}
+			try (PersonalStorage pst = PersonalStorage.fromFile(inputFile)) {
+			    exploreCorruptedPst(debug, pst, pst.getRootFolder().getEntryIdString(), dataDir);
+			} catch (Exception e) {
+				System.out.printf("ERROR: %s", e);
+				System.exit(2);
+			}
+		} else {
+			if ( fOut.exists() || fOut.isFile() ) {
+				String sOutErr = String.format("ERROR: output file \"%s\" already exists", outputFile);
+				System.out.println(sOutErr);
+				System.exit(2);
+			}
+			// Read an OST file
+			readAnOSTFile(debug, inputFile);
+			// Converting OST to PST
+			convertOSTToPST(debug, inputFile, outputFile);
 		}
-
-		//Read an OST file
-		readAnOSTFile(debug, inputFile);
-		//Converting OST to PST
-		convertOSTToPST(debug, inputFile, outputFile);
 	}
 
 	public static void readAnOSTFile(final boolean debug, String inputFile) {
 		// Load the Outlook PST file
-		File fIn = new File(inputFile);
 		PersonalStorage pst = null;
 		FolderInfoCollection folderInfoCollection = null;
-		String sLoad = String.format("INFO: Loading OST file \"%s\" (%s)", inputFile, humanReadableByteCount(fIn.length(), true));
-		System.out.println(sLoad);
 		try {
 			pst = PersonalStorage.fromFile(inputFile);
 		} catch (Exception e) {
 			System.out.printf("ERROR: %s", e);
 		}
 		folderInfoCollection = pst.getRootFolder().getSubFolders();
-		// Loop over all the-sub folders
-		for (int i = 0; i < folderInfoCollection.size(); i++) {
+		// Loop over all sub-folders
+		for (int i = 1; i < folderInfoCollection.size(); i++) {
 			// Display all the folders
 			FolderInfo folderInfo = folderInfoCollection.get_Item(i);
-			System.out.printf("%5s Folder [%02d] \"%s\"\n", " ", i, folderInfo.getDisplayName());
+			System.out.printf("%4s [%02d] Folder \"%s\"\n", " ", i, folderInfo.getDisplayName());
 		}
 		System.out.println("");
 	}
@@ -115,9 +171,8 @@ public class LoadAndConvertOSTFileCLI {
 		return (int) fileTypeBytes[0];
 	}
 
-	public static void convertOSTToPST(final boolean debug, final String inputFile, final String outputFile) {
-		// Get file format type: array index is 'wVer' and maps to description String 
-		int fmtTypeNum = typeOSTFile(debug, inputFile);
+	// Get file format type: array index is 'wVer' and maps to description String
+	public static String fileFormatType(final boolean debug, int fmtTypeNum) {
 		String[] arr = new String[50];
 		String fmtTypeStr = "Unknown";
 		arr[14] = "32-bit ANSI";
@@ -125,11 +180,12 @@ public class LoadAndConvertOSTFileCLI {
 		arr[21] = "64-bit Unicode, Visual Recovery";
 		arr[23] = "64-bit Unicode";
 		arr[36] = "64-bit Unicode 4k, Outlook 2013";
-		if (arr[fmtTypeNum] != null) {
-			fmtTypeStr = arr[fmtTypeNum];
-		}
-		System.out.printf("INFO: File format is \"%s\" (%d)\n", fmtTypeStr, fmtTypeNum);
-		if (fmtTypeNum != 36) {
+		if (arr[fmtTypeNum] != null) fmtTypeStr = arr[fmtTypeNum];
+		return (String) fmtTypeStr;
+	}
+
+	public static void convertOSTToPST(final boolean debug, final String inputFile, final String outputFile) {
+		if (typeOSTFile(debug, inputFile) != 36) {
 			// Prepare task to convert and save
 	        Runnable r = new Runnable() {
 	            public void run() {
@@ -155,9 +211,7 @@ public class LoadAndConvertOSTFileCLI {
 			int i = 0;
 			System.out.printf("INFO: Converting \"%s\" to \"%s\"\n", inputFile, outputFile);
 			while (fIn.length() > fOut.length()) {
-				if (debug) {
-					System.out.println("DEBUG: fIn.length=" + fIn.length() + " fOut.length=" + fOut.length());
-				}
+				if (debug) System.out.println("DEBUG: fIn.length=" + fIn.length() + " fOut.length=" + fOut.length());
 				if ((int)Math.round((100.0 * fOut.length() / fIn.length())) >= 99) {
 					spin = String.format("%16s", " ");
 				}
@@ -167,11 +221,7 @@ public class LoadAndConvertOSTFileCLI {
 					(int)Math.round((100.0 * fOut.length() / fIn.length())),
 					spin.charAt(i % 16)
 				);
-				int sLen = (int)(80 - sConv.length());
-				if (sLen != 0) {
-					String sPad = String.format("%" + sLen + "s", " ");
-					System.out.print(sConv + sPad + "\r");
-				}
+				System.out.print(sameLine(sConv));
 	            i++;
 			}
 			System.out.println("");
@@ -182,11 +232,114 @@ public class LoadAndConvertOSTFileCLI {
 	}
 	
 	/* http://programming.guide/java/formatting-byte-size-to-human-readable-format.html */
-	public static String humanReadableByteCount(long bytes, boolean si) {
-	    int unit = si ? 1000 : 1024;
-	    if (bytes < unit) return bytes + "B";
-	    int exp = (int) (Math.log(bytes) / Math.log(unit));
-	    String pre = (si ? "kMGTPE" : "KMGTPE").charAt(exp-1) + (si ? "" : "i");
-	    return String.format("%.0f%sB", bytes / Math.pow(unit, exp), pre);
-	}	  
+	public static String humanReadableByteCountSI(long bytes) {
+	    if (-1000 < bytes && bytes < 1000) {
+	        return bytes + " B";
+	    }
+	    CharacterIterator ci = new StringCharacterIterator("kMGTPE");
+	    while (bytes <= -999_950 || bytes >= 999_950) {
+	        bytes /= 1000;
+	        ci.next();
+	    }
+	    return String.format("%.1f %cB", bytes / 1000.0, ci.current());
+	}
+
+	// Get random number 97-122 (ascii lc letters)
+	public static int getRnd() {
+		return (int)(Math.random()*(25))+97;
+	}
+
+	// Generate random output: "12345abcde"
+	public static String getRndName() {
+        int rndNumbers = getRnd();
+		String rndLetters = "";
+		for (int i=0; i < 5; i++) {
+	        rndNumbers += getRnd();
+	        rndLetters += (char) getRnd();
+		}
+	    return String.format("%s%s", rndNumbers*getRnd(), rndLetters);
+	}
+
+	// String on same line, 80 chars wide
+	public static String sameLine(String text) {
+		int sLen = (int)(80 - text.length());
+		if (sLen != 0) {
+			String sPad = String.format("%" + sLen + "s", " ");
+			//return String.format(text.substring(0, Math.min(text.length(), 80)) + sPad + "\r");
+			return text + sPad + "\r";
+		}
+		return null;
+	}
+	
+	public class Items {
+		private int cnt = 0;
+		public void setCnt(int cnt) {
+			this.cnt = cnt;
+		}
+		public int getCnt() {
+		    return this.cnt;
+		}
+	}
+
+	public static void exploreCorruptedPst(final boolean debug, PersonalStorage pst, String rootFolderId, String dataDir) {
+
+		LoadAndConvertOSTFileCLI outerObj = new LoadAndConvertOSTFileCLI();  	
+		LoadAndConvertOSTFileCLI.Items numMsg = outerObj.new Items();
+		LoadAndConvertOSTFileCLI.Items numMsgErr = outerObj.new Items();
+		LoadAndConvertOSTFileCLI.Items numFolder = outerObj.new Items();    	
+		numFolder.cnt = 1;
+		numMsg.cnt = 1;
+
+		Iterable<String> messageIdList = pst.findMessages(rootFolderId);
+
+        for (String messageId : messageIdList) {
+	        try {
+	            MapiMessage msg = pst.extractMessage(messageId);
+
+	            // TODO: get body as text, add option
+	            //System.out.printf(msg.getBody());
+	            if (dataDir != null) {
+		            // Save msg file, dont use '/' in filename
+		            String msgFile = dataDir + "/" + messageId.replace("/", "_") + ".msg";
+		            msg.save(msgFile);
+		            //System.out.print(msgInfo);
+		            System.out.print(sameLine(String.format("%5s [%02d/50] File: \"%s\"\r", " ", numMsg.getCnt(), msgFile)));
+	            } else {
+	            	System.out.printf("%5s [%02d/50] Subject: \"%s\"\n", " ", numMsg.getCnt(), msg.getSubject());
+	            }
+	            numMsg.cnt++;
+	            numMsg.setCnt(numMsg.cnt);
+	        } catch (Exception e) {
+	        	if (numMsgErr.getCnt() > 100) {
+	        		System.out.printf("\nMore than 100 unreadable items found, exiting...\n");
+	        		System.exit(1);
+	        	}
+	            System.out.printf("\\nMessage reading error. Entry id: %s\\n", messageId);
+		        numMsgErr.cnt++;
+		        numMsgErr.setCnt(numMsgErr.cnt);
+	        }
+	    }
+
+	    Iterable<String> folderIdList = pst.findSubfolders(rootFolderId);
+
+	    // TODO: get/set folder to use in messages loop above
+	    for (String subFolderId : folderIdList) {
+	        if (subFolderId != rootFolderId) {
+	            try {
+	                FolderInfo subfolder = pst.getFolderById(subFolderId);
+	                if (subfolder.getDisplayName().toString() != "") {
+	                	System.out.printf("%5s [%02d] Folder: \"%s\"%8s\n", " ", numFolder.getCnt(), subfolder.getDisplayName(), " ");
+	                	numFolder.cnt++;
+	                	numFolder.setCnt(numFolder.cnt);
+	                } else {
+	                	System.out.printf("ERROR: could not display [%02d] subfolder=%s subFolderIdid=%s\n", numFolder.getCnt(), subfolder, subFolderId);
+	                }
+	            } catch (Exception e) {
+	                System.out.printf("\nFolder reading error. Entry id: %s\n", subFolderId);
+	            }
+	            exploreCorruptedPst(debug, pst, subFolderId, dataDir);
+	        }
+	    }
+	}
+
 }
